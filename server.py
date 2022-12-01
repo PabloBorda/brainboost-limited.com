d7cb9cd98ab7c4163bec5ddeadf1f69640d7ed20
brainboost <brainboost@brainboost.ie>
2022-12-01 12:26:16 +0000

    ssl configured

--- server.py
+++ server.py
@@ -1,18 +1,20 @@
 # encode: utf-8
 import cherrypy
 
-WEB_ROOT = "/Users/pablotomasborda/Desktop/www.brainboost-limited.com/static/"
+WEB_ROOT = "/root/www.brainboost-limited.com/static"
 class CServer( object ) :
     @cherrypy.expose
     def do_contact(self, **params):
         pass
     @cherrypy.expose
     def enter_with_password(self,**params):
-        with open("static/index.html", "r") as file:
-            data = str(file.read())
+        if params['pwd']=='probando':
+            with open("static/index.html", "r") as file:
+                data = str(file.read())
 
-        return data
-
+            return data
+        else:
+            return 'False'
 
 
 cherrypy.quickstart(CServer(), '/',"cherrypy.cfg")
